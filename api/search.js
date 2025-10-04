da9eeef8525ba4a2fa703a063578aef2091937d0
Rehan Dev <121321439+R3HAN6@users.noreply.github.com>
2025-10-04 18:35:47 +0800

    search.js

--- api/search.js
+++ api/search.js
@@ -1,34 +1,39 @@
 import axios from 'axios';
-import { token } from './get-token.js'; 
+import { token } from './get-token.js';
 
-try {
-const gettoken = await token();
-const url = "https://sapi.dramaboxdb.com/drama-box/search/suggest";
+export default async function handler(req, res) {
+  try {
+    const gettoken = await token();
+    const url = "https://sapi.dramaboxdb.com/drama-box/search/suggest";
 
-const headers = {
-  "User-Agent": "okhttp/4.10.0",
-  "Accept-Encoding": "gzip",
-  "Content-Type": "application/json",
-  "tn": `Bearer ${gettoken.token}`,
-  "version": "430",
-  "vn": "4.3.0",
-  "cid": "DRA1000042",
-  "package-name": "com.storymatrix.drama",
-  "apn": "1",
-  "device-id": gettoken.deviceid,
-  "language": "in",
-  "current-language": "in",
-  "p": "43",
-  "time-zone": "+0800",
-  "content-type": "application/json; charset=UTF-8"
-};
+    const headers = {
+      "User-Agent": "okhttp/4.10.0",
+      "Accept-Encoding": "gzip",
+      "Content-Type": "application/json",
+      "tn": `Bearer ${gettoken.token}`,
+      "version": "430",
+      "vn": "4.3.0",
+      "cid": "DRA1000042",
+      "package-name": "com.storymatrix.drama",
+      "apn": "1",
+      "device-id": gettoken.deviceid,
+      "language": "in",
+      "current-language": "in",
+      "p": "43",
+      "time-zone": "+0800",
+      "content-type": "application/json; charset=UTF-8"
+    };
 
-const data = {
-    keyword: "pewaris", // keyword pencarian
-};
+    // Ambil keyword dari query string, default "pewaris" kalau kosong
+    const keyword = req.query.query || "pewaris";
 
-const res = await axios.post(url, data, { headers })
-console.log(res.data.data.suggestList); // output list drama yang dicari
-} catch (error) {
-    console.error(error);
+    const data = { keyword };
+
+    const response = await axios.post(url, data, { headers });
+    
+    res.status(200).json(response.data.data.suggestList); // kirim ke client
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
 }
